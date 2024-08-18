ade809ee9249d38707dac1da05cda40020ff9ec0
Georginio-prod <etonameklou6@gmail.com>
2024-08-18 23:19:02 +0000

    style: add the composent for feature

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import navbar from './components/navbar.vue'
 import ContentD from './components/contentD.vue';
+import feature from './components/feature.vue';
 </script>
 
 <template>
   <navbar/>
   <main>
     <ContentD/>
+    <feature/>
   </main>
 </template>
 
---
+++ src/components/feature.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex w-full items-center mt-32 justify-center flex-col px-6 xl:mt-10">
+        <div class="text-center  text-tcol">
+            <p class="xl:font-medium xl:text-3xl xl:pb-4 text-2xl">
+                Features
+            </p>
+            <h1 class="xl:px-96 xl:text-lg opacity-50 text-sm pt-2">
+                Our aim is to make it quick and easy for you to access your favourite websites.
+                Your bookmarks sync between your devices so you can access them on the go.
+            </h1>
+        </div>
+    </div>
+</template>
